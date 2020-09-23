dcd4a29256e2499b26d2d03be509a2cdb3534c63
Mike Chen <ccmikechen@gmail.com>
2020-09-23 18:11:11 +0800

    Add docker-compose.yml

--- server/index.js
+++ server/index.js
@@ -18,9 +18,11 @@ const pgClient = new Pool({
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
+});
 
 const redis = require('redis');
 const redisClient = redis.createClient({
